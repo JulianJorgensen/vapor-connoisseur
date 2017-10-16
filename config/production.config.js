6ea19186254ed3de1da6da190fcf04aeda7b54b5
Julian Jorgensen <me@julianjorgensen.com>
2017-10-16 22:44:45 +0800

    update env config with shopify settings for prod

--- config/production.config.js
+++ config/production.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   ENV: "production",
   SITE_URL: "https://julianjorgensen.herokuapp.com",
+  SHOPIFY: {
+    DOMAIN: "vapor-connoisseur.myshopify.com",
+    STORE_FRONT_ACCESS_TOKEN: "d16e6aa0ee8ac23734c065c9379f2692"
+  }
 };
