83aaeb1661b3d6f71c105683400ede53ea110fa8
Julian Jorgensen <me@julianjorgensen.com>
2017-10-16 22:56:26 +0800

    use cors

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ let expressStaticGzip = require("express-static-gzip");
 
 // cors
 let cors = require("cors");
-app.use(cors());
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://vapor-connoisseur.herokuapp.com/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // routes
 let contentful = require('./server/routes/contentful');
