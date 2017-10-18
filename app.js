5473c61ea855cf477c2976c053b0a4d1ae6e5c61
Julian Jorgensen <me@julianjorgensen.com>
2017-10-18 18:00:53 +0800

    restructure server code

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ let app = module.exports = express();
 let logger = require('morgan');
 let expressStaticGzip = require("express-static-gzip");
 let cors = require("cors");
+let util = require('./server/util/util');
+let ENV_CONFIG = util.getEnvConfig();
 
 // routes
 let contentful = require('./server/routes/contentful');
-let email = require('./server/routes/email');
-
-let util = require('./server/util/util');
-let ENV_CONFIG = util.getEnvConfig();
 
 // Set port
 app.set('port', (process.env.PORT || 3000));
@@ -21,24 +19,12 @@ app.use(logger('dev')); // Log requests to API using morgan
 // cors
 app.use(cors());
 
-//CORS middleware
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://vapor-connoisseur.herokuapp.com/');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-  next();
-}
-app.use(allowCrossDomain);
-
-// Content routes
-app.use('/contentful', contentful);
-
-// Email routes
-app.use('/email', email);
-
 // Serve Gzip
 app.use("/", expressStaticGzip(__dirname + '/public'));
+
+
+// Contentful routes
+app.get('/contentful/getAllContent', contentful.getAllContent);
 
 // Catch all other paths and serve the index file
 app.all('*', function(request, response) {
--- server/routes/contentful.js
+++ server/routes/contentful.js
@@ -1,20 +1,12 @@
-let express = require('express');
-let router = express.Router();
-let app = require('../../app');
-let util = require('../util/util');
-
 import { createClient } from 'contentful';
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
 });
 
-router.route('/getAllContent')
-  .get((req, res) => {
-    client.getEntries().then((response) => {
-        res.status(200).send(response.items);
-      })
-      .catch(console.error);
-  });
-
-module.exports = router;
+exports.getAllContent = function(req, res){
+  client.getEntries().then((response) => {
+    res.status(200).send(response.items);
+  })
+  .catch(console.error);
+};
