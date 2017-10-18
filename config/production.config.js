19edccc04fb3b0b4147ee89041ed6376752e1dd0
Julian Jorgensen <me@julianjorgensen.com>
2017-10-18 18:09:38 +0800

    restructure server code

--- config/production.config.js
+++ config/production.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   ENV: "production",
-  SITE_URL: "https://julianjorgensen.herokuapp.com",
+  SITE_URL: "https://vapor-connoisseur.herokuapp.com",
   SHOPIFY: {
     DOMAIN: "vapor-connoisseur.myshopify.com",
     STORE_FRONT_ACCESS_TOKEN: "d16e6aa0ee8ac23734c065c9379f2692"
