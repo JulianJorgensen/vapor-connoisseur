45952319ed349b86ad68850519327e302b026711
Julian Jorgensen <me@julianjorgensen.com>
2017-11-07 15:02:12 +0800

    Fix upload styles

--- client/components/Upload/index.js
+++ client/components/Upload/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import FileInput from 'react-file-input';
+import { ThreeBounce } from 'better-react-spinkit';
 import styles from './index.css';
 
 export default class Upload extends Component {
@@ -8,16 +9,29 @@ export default class Upload extends Component {
 
   uploadFile(e) {
     e.preventDefault();
+    this.setState({
+      uploading: true,
+    });
     const file = e.target.files[0];
     const data = new FormData();
     data.append('file', file);
     axios.post('/upload', data).then((response) => {
       console.log('response', response);
       this.props.input.onChange(response.data);
+      this.setState({
+        uploading: false,
+      });
     });
   }
 
   render() {
+    const renderUploading = () => (
+      <div className={styles.uploading}>
+        <div>Uploading</div>
+        <ThreeBounce size={15} color="white" />
+      </div>
+    );
+
     return (
       <div className={styles.wrapper}>
         <FileInput
@@ -26,8 +40,9 @@ export default class Upload extends Component {
           accept=".png,.gif,.jpg,.jpeg,.pdf,.ai,.psd,.svg"
           placeholder="Upload logo"
           onChange={e => this.uploadFile(e)}
-          disabled={this.props.disabled}
+          disabled={this.props.disabled || this.state.uploading}
         />
+        {this.state.uploading ? renderUploading() : ''}
       </div>
     );
   }
