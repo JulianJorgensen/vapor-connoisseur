4932c6353efbd15f4bbaae062febd85336966435
Julian Jorgensen <me@julianjorgensen.com>
2017-11-07 13:35:48 +0800

    Rename staging config

---
+++ config/staging.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  ENV: 'staging',
+  SITE_URL: 'https://vapor-connoisseur-staging.herokuapp.com',
+  SHOPIFY: {
+    DOMAIN: 'vapor-connoisseur.myshopify.com',
+    STORE_FRONT_ACCESS_TOKEN: 'd16e6aa0ee8ac23734c065c9379f2692',
+  },
+  EMAILS: {
+    ADMIN: {
+      name: 'Julian Jorgensen', address: 'me@julianjorgensen.com',
+    },
+    CC: [
+      {
+        name: 'Julian Jorgensen', address: 'me@julianjorgensen.com',
+      },
+    ],
+  },
+};
